fcd2bb8efdd6ff143ed6db0039cc4976eeb3d6aa
Lyndon <lynndon@gmail.com>
2026-05-29 13:20:42 +0800

    refactor: update to upstream 6.1.0
    - Changed VK_ROOT_BYTES initialization in the verifier module.
    - Updated the SP1CompressedVerifier to check public values length and verify VK merkle root.
    - Adjusted data types in load_plonk_verifying_key_from_bytes for better accuracy.
    - Incremented offset calculations for commitment constraint indexes.
    - Updated binary verification key artifacts.

--- crates/verifier/src/plonk/converter.rs
+++ crates/verifier/src/plonk/converter.rs
@@ -217,17 +217,13 @@ pub(crate) fn load_plonk_verifying_key_from_bytes(
     // Original: unchecked_compressed_x_to_g2_point(&buffer[offset + 96..offset + 160])?
     let g2_1 = parse_compressed_g2(&buffer[offset + 96..offset + 160])?;
 
-    offset += 160 + 33788;
+    offset += 160 + 33792;
 
-    let num_commitment_constraint_indexes = u64::from_be_bytes([
+    let num_commitment_constraint_indexes = u32::from_be_bytes([
         buffer[offset],
         buffer[offset + 1],
         buffer[offset + 2],
         buffer[offset + 3],
-        buffer[offset + 4],
-        buffer[offset + 5],
-        buffer[offset + 6],
-        buffer[offset + 7],
     ]) as usize;
 
     // Verifying key for SP1 proofs have this size.
@@ -236,7 +232,7 @@ pub(crate) fn load_plonk_verifying_key_from_bytes(
     }
 
     let mut commitment_constraint_indexes = Vec::new();
-    offset += 8;
+    offset += 4;
     for _ in 0..num_commitment_constraint_indexes {
         let index = u64::from_be_bytes([
             buffer[offset],
